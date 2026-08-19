8fe672df70dda07199f846eee61242645e8fa84f
Afroz Shaik <affrozali9342@gmail.com>
2026-08-19 08:40:21 +0530

    Added health route

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRoutes=require("./routes/auth.routes")
-const passport=require("./config/passport")
-const conversationRoutes=require("./routes/conversation.routes")
-const messageRoutes=require("./routes/message.routes")
+const authRoutes = require("./routes/auth.routes")
+const passport = require("./config/passport")
+const conversationRoutes = require("./routes/conversation.routes")
+const messageRoutes = require("./routes/message.routes")
 const userRoutes = require("./routes/user.routes");
 const app = express();
 
@@ -19,15 +19,23 @@ app.use(
 );
 app.use(cookieParser());
 
-// Health Check Route
+// Health Check Route  for uptime Robot
+
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
     message: "Chat Application Backend Running",
   });
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+  });
+});
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/conversations",conversationRoutes);
-app.use("/api/v1/messages",messageRoutes)
+app.use("/api/v1/conversations", conversationRoutes);
+app.use("/api/v1/messages", messageRoutes)
 app.use("/api/v1/users", userRoutes);
 module.exports = app;
